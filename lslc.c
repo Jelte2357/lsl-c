d5e17786fd6427bdeb99744841cb4c63fd21eea2
Jelte2357 <86104076+Jelte2357@users.noreply.github.com>
2024-04-26 10:01:00 +0200

    Made it more readable
    Also it uses a to_uri function now so it may be marginally faster, though the compiler most likely already dit this.

--- lslc.c
+++ lslc.c
@@ -4,27 +4,44 @@
 #include <string.h>
 
 
+char* to_uri(const int folder, const char* fileName) { // Convert a file path to a URI (that would still need file:// in front of it to be a valid URI)
+    char curpath[MAX_PATH];
+    GetCurrentDirectoryA(MAX_PATH, curpath); 
+    strcat(curpath, "/");
+    if (folder == 0) {
+        strcat(curpath, fileName);
+    }
+    for (int i = 0; i < strlen(curpath); i++) { //replace \ with / and spaces with %20
+        if (curpath[i] == '\\') {
+            curpath[i] = '/';
+        }
+        if (curpath[i] == ' ') {
+            curpath[i] = '%';
+            for (int j = strlen(curpath); j > i; j--) {
+                curpath[j + 2] = curpath[j];
+            }
+            curpath[i + 1] = '2';
+            curpath[i + 2] = '0';
+        }
+    }
+    return strdup(curpath);
+}
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int folder = 0;
     int sort = 0;
     int amount = 0;
 
-    for (int i = 0; i < argc; i++)
-    {
-        if (strcmp(argv[i], "-f") == 0 || strcmp(argv[i], "--folder") == 0)
-        {
+    for (int i = 0; i < argc; i++) { //parse arguments
+        if (strcmp(argv[i], "-f") == 0 || strcmp(argv[i], "--folder") == 0) {
             folder = 1;
             amount++;
         }
-        if (strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--sorted") == 0)
-        {
+        if (strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--sorted") == 0) {
             sort = 1;
             amount++;
         }
-        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
-        {
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
             printf("Usage: lsl [-f] [-s] <location>\n");
             return 0;
         }
@@ -37,28 +54,23 @@ int main(int argc, char *argv[])
 
     char *location;
 
-    if (amount < argc - 1)
-    {
+    if (amount < argc - 1) { //parse location
         location = argv[argc - 1];
     }
 
-    else
-    {
+    else {
         location = ".";
     }
 
-    while (location[0] == '/' || location[0] == '\\')
-    {
+    while (location[0] == '/' || location[0] == '\\') { //lstrip slashes
         location++;
     }
 
-    if (strcmp(location,"")==0)
-    {
+    if (strcmp(location,"")==0) {
         location = ".";
     }
     
-    if (SetCurrentDirectoryA(location) == 0)
-    {
+    if (SetCurrentDirectoryA(location) == 0) { //cd to location (easier that to deal with path myself)
         printf("Error: Failed to open directory\n");
         return 1;
     }
@@ -69,154 +81,48 @@ int main(int argc, char *argv[])
     char *searchPath = ".\\*";
 
     hFind = FindFirstFileA(searchPath, &findData);
-    if (hFind == INVALID_HANDLE_VALUE)
-    {
+    if (hFind == INVALID_HANDLE_VALUE) {
         printf("Error: Failed to open directory\n");
         return 1;
     }
-    if (sort == 0)
-    {
-        do
-        {
-            if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
-            {
-                if (strcmp(findData.cFileName, ".") != 0 && strcmp(findData.cFileName, "..") != 0)
-                {
-                    char curpath [MAX_PATH];
-                    GetCurrentDirectoryA(MAX_PATH, curpath);
-                    strcat(curpath, "\\");
-                    strcat(curpath, findData.cFileName);
-                    for (int i = 0; i < strlen(curpath); i++)
-                    {
-                        if (curpath[i] == '\\')
-                        {
-                            curpath[i] = '/';
-                        }
-                    }
-                    for (int i = 0; i < strlen(curpath); i++)
-                    {
-                        if (curpath[i] == ' ')
-                        {
-                            curpath[i] = '%';
-                            for (int j = strlen(curpath); j > i; j--)
-                            {
-                                curpath[j + 2] = curpath[j];
-                            }
-                            curpath[i + 1] = '2';
-                            curpath[i + 2] = '0';
-                        }
-                    }
-                    printf("\033]8;;file:///%s\033\\\033[34m%s\033[0m\033]8;;\033\\\n", curpath, findData.cFileName);
+    if (sort == 0) { //if not sorted, print files and directories in alphabetical order
+        do {
+            if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+                if (strcmp(findData.cFileName, ".") != 0 && strcmp(findData.cFileName, "..") != 0) {
+                    char* results = to_uri(0, findData.cFileName);
+                    printf("\033]8;;file:///%s\033\\\033[34m%s\033[0m\033]8;;\033\\\n", results, findData.cFileName); //blue clickable link
+                    free(results);
                 }
             }
-            else
-            {
-                char curpath [MAX_PATH];
-                GetCurrentDirectoryA(MAX_PATH, curpath);
-                strcat(curpath, "\\");
-                if (folder == 0) {
-                    strcat(curpath, findData.cFileName);
-                }
-                for (int i = 0; i < strlen(curpath); i++)
-                {
-                    if (curpath[i] == '\\')
-                    {
-                        curpath[i] = '/';
-                    }
-                }
-                for (int i = 0; i < strlen(curpath); i++)
-                {
-                    if (curpath[i] == ' ')
-                    {
-                        curpath[i] = '%';
-                        for (int j = strlen(curpath); j > i; j--)
-                        {
-                            curpath[j + 2] = curpath[j];
-                        }
-                        curpath[i + 1] = '2';
-                        curpath[i + 2] = '0';
-                    }
-                }
-                printf("\033]8;;file:///%s\033\\%s\033]8;;\033\\\n", curpath, findData.cFileName);
+            else {
+                char* results = to_uri(folder, findData.cFileName);
+                printf("\033]8;;file:///%s\033\\%s\033]8;;\033\\\n", results, findData.cFileName); //white clickable link
+                free(results);
             }
         } while (FindNextFileA(hFind, &findData));
     }
 
-    else if (sort == 1)
-    {
-        do
-        {
-            if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
-            {
-                if (strcmp(findData.cFileName, ".") != 0 && strcmp(findData.cFileName, "..") != 0)
-                {
-                                    char curpath [MAX_PATH];
-                GetCurrentDirectoryA(MAX_PATH, curpath);
-                strcat(curpath, "\\");
-                strcat(curpath, findData.cFileName);
-                for (int i = 0; i < strlen(curpath); i++)
-                {
-                    if (curpath[i] == '\\')
-                    {
-                        curpath[i] = '/';
-                    }
-                }
-                for (int i = 0; i < strlen(curpath); i++)
-                {
-                    if (curpath[i] == ' ')
-                    {
-                        curpath[i] = '%';
-                        for (int j = strlen(curpath); j > i; j--)
-                        {
-                            curpath[j + 2] = curpath[j];
-                        }
-                        curpath[i + 1] = '2';
-                        curpath[i + 2] = '0';
-                    }
-                }
-                printf("\033]8;;file:///%s\033\\\033[34m%s\033[0m\033]8;;\033\\\n", curpath, findData.cFileName);
+    else if (sort == 1) { //if sorted, print directories first, then files
+        do {
+            if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+                if (strcmp(findData.cFileName, ".") != 0 && strcmp(findData.cFileName, "..") != 0) {
+                    char* results = to_uri(0, findData.cFileName);
+                    printf("\033]8;;file:///%s\033\\\033[34m%s\033[0m\033]8;;\033\\\n", results, findData.cFileName); //blue clickable link
+                    free(results);
                 }
             }
         } while (FindNextFileA(hFind, &findData));
         FindClose(hFind);
         hFind = FindFirstFileA(searchPath, &findData);
-        do
-        {
-            if (!(findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
-            {
-                char curpath [MAX_PATH];
-                GetCurrentDirectoryA(MAX_PATH, curpath);
-                strcat(curpath, "\\");
-                if (folder == 0) {
-                    strcat(curpath, findData.cFileName);
-                }
-                for (int i = 0; i < strlen(curpath); i++)
-                {
-                    if (curpath[i] == '\\')
-                    {
-                        curpath[i] = '/';
-                    }
-                }
-                for (int i = 0; i < strlen(curpath); i++)
-                {
-                    if (curpath[i] == ' ')
-                    {
-                        curpath[i] = '%';
-                        for (int j = strlen(curpath); j > i; j--)
-                        {
-                            curpath[j + 2] = curpath[j];
-                        }
-                        curpath[i + 1] = '2';
-                        curpath[i + 2] = '0';
-                    }
-                }
-                printf("\033]8;;file:///%s\033\\%s\033]8;;\033\\\n", curpath, findData.cFileName);
+        do {
+            if (!(findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+                char* results = to_uri(folder, findData.cFileName);
+                printf("\033]8;;file:///%s\033\\%s\033]8;;\033\\\n", results, findData.cFileName); //white clickable link
+                free(results);
             }
         } while (FindNextFileA(hFind, &findData));
     }
 
     FindClose(hFind);
-
-
     return 0;
 }
